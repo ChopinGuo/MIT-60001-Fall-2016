7ac88c13680dc5d47df49810725c6caa1459d2f7
ChopinGuo <guoshaopan@pku.edu.cn>
2021-01-15 19:56:28 -0600

    Create Ch17_tuplesIntroduction.py

---
+++ Ch17/Ch17_tuplesIntroduction.py
@@ -0,0 +1,9 @@
+# Ch17 Tuples
+te = ()
+print(te)          # -> ()
+print(type(te))    # -> <class 'tuple'>
+print(len(te))     # -> 0
+
+t = (2, "chopin", 3)
+print(t)        # evaluates to (2, 'chopin', 3)
+print(t[0])     # # evaluates to 2
